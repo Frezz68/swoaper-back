62f1503c1c82f06ed2a2e7cebebd9c73800b2974
Alan <alan.jordy1999@gmail.com>
2023-03-22 14:47:06 +0100

    🚧 Add contact form page

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact_page')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
+}
